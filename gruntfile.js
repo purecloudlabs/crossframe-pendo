fc86e2794f7b3b243c68691260566edda3bdbbd1
Mike McAuley <mike.mcauley@inin.com>
2016-09-01 15:59:14 -0400

    Finished initial grunt config

---
+++ gruntfile.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    webpack: {
+      build: {
+        entry: './src/crossframe-pendo.js',
+        output: {
+          path: './dist/',
+          filename: 'crossframe-pendo.js',
+          library: 'crossframePendo',
+          libraryTarget: 'umd'
+        },
+        module: {
+          loaders: [{
+            loader: 'babel-loader'
+          }]
+        }
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true,
+        sourceMapName: './dist/crossframe-pendo.map'
+      },
+      build: {
+        files: {
+          './dist/crossframe-pendo.min.js': './dist/crossframe-pendo.js'
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: [
+          './src/**/*.js'
+        ],
+        tasks: [
+          'webpack',
+          'uglify'
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['webpack', 'uglify', 'watch']);
+
+};
