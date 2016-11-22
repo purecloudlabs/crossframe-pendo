70a8d93eb2f34a3221daef296f29837f855d401b
Mike McAuley <mike.mcauley@inin.com>
2016-11-22 10:55:31 -0500

    refactor rpc module to use new classes

--- src/lib/rpc.js
+++ src/lib/rpc.js
@@ -1,15 +1,15 @@
+let IncomingRequest = require('./incoming-request');
+let OutgoingRequest = require('./outgoing-request');
 let pendoActions = require('./pendo-actions');
 let Promise = require('es6-promise').Promise;
+let RequestList = require('./request-list');
 
 // config values
 const PROTOCOL = 'crossframe-pendo';
-const RPC_TIMEOUT = 5000;
 
-// resolve/reject methods for promises to be fulfilled by RPC responses
-let responseCallbacks = []
-
-// keep track of unfulfilled RPC requests
-let activeRequests = []
+// keep track of incoming & outgoing requests
+let incomingRequests = new RequestList();
+let outgoingRequests = new RequestList();
 
 // build list of all immediate parent and/or child frames
 function getAdjacentFrames () {
@@ -43,116 +43,84 @@ function handleMessage (messageEvent) {
 // handle incoming RPC request
 function handleRpcRequest (messageEvent) {
   let messageData = messageEvent.data;
-  let thisRequest = {
-    source: messageEvent.source,
-    method: messageData.method,
-    args: messageData.args
-  }
-  activeRequests.push(thisRequest);
-  pendoActions[messageData.method].apply(null, messageData.args)
-  .then(function (result) {
-    sendRpcResponse(messageEvent.source, messageData.id, true);
-    activeRequests = activeRequests.filter(function (request) {
-      return request !== thisRequest;
-    });
-  })
-  .catch(function (error) {
-    sendRpcResponse(messageEvent.source, messageData.id, false);
-    activeRequests = activeRequests.filter(function (request) {
-      return request !== thisRequest;
-    });
-  });
+  let requestAttrs = {method: messageData.method, args: messageData.args};
+  let incomingRequest = new IncomingRequest(requestAttrs, messageEvent.source);
+  incomingRequests.add(incomingRequest);
+  pendoActions[messageData.method].apply(null, messageData.args);
 }
 
 // handle incoming RPC response
 function handleRpcResponse (messageEvent) {
   let messageData = messageEvent.data;
+  let requestAttrs = {method: messageData.method, args: messageData.args};
   if (messageData.success) {
-    responseCallbacks[messageData.id].resolve();
-  } else {
-    responseCallbacks[messageData.id].reject();
+    resolveRequest(messageData.method, messageData.args);
+  }
+}
+
+// try/catch wrapper for window.postMessage
+function postMessage (frame, message) {
+  try {
+    frame.postMessage(message, frame.location.origin);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// post message and/or resolve promise as needed to resolve oustanding requests
+function resolveRequest (method, args) {
+  let requestAttrs = {method: method, args: args};
+  if (incomingRequest = incomingRequests.find(requestAttrs)) {
+    sendRpcResponse(incomingRequest.contentWindow, method, args, true);
+    incomingRequests.remove(incomingRequest);
+  }
+  if (outgoingRequest = outgoingRequests.find(requestAttrs)) {
+    outgoingRequest.resolve();
+    outgoingRequests.remove(outgoingRequest);
   }
 }
 
 // send an RPC request
 function sendRpcRequest (frame, method, args) {
-  let response = new Promise(function (resolve, reject) {
-    responseCallbacks.push({
-      resolve: resolve,
-      reject: reject
-    });
-    window.setTimeout(reject, RPC_TIMEOUT);
-  });
-  let callbackId = responseCallbacks.length - 1;
   let message = {
     protocol: PROTOCOL,
     type: 'rpc:request',
-    id: callbackId,
     method: method,
     args: args
   }
-  try {
-    frame.postMessage(message, frame.location.origin);
-  } catch (e) {
-    responseCallbacks[callbackId].reject('UNABLE_TO_POSTMESSAGE');
-  }
-  return response;
+  postMessage(frame, message);
 }
 
 // send an RPC response
-function sendRpcResponse (frame, respondingTo, didSucceed) {
+function sendRpcResponse (frame, method, args, didSucceed) {
   let message = {
     protocol: PROTOCOL,
     type: 'rpc:response',
-    id: respondingTo,
+    method: method,
+    args: args,
     success: didSucceed
   }
-  frame.postMessage(message, frame.location.origin);
+  postMessage(frame, message);
 }
 
 // attempt rpc in all adjacent frames, resolve on any successful attempt
-function tryAdjacentFrames (method, args) {
-  return new Promise(function (resolve, reject) {
-    let failedAttempts = 0;
-    let adjacentFrames = getAdjacentFrames().filter(function (frame) {
-
-      // prevent sending same request back to original window
-      for (let activeRequest of activeRequests) {
-        if (activeRequest.source === frame) {
-          if (activeRequest.method === method) {
-            if (activeRequest.args.length === args.length) {
-              let argsMatch = false;
-              for (let i = 0; i < args.length; i++) {
-                if (activeRequests.args[i] === args[i]) {
-                  argsMatch = true;
-                }
-              }
-              if (argsMatch) {
-                return false;
-              }
-            }
-          }
-        }
-      }
-
+function tryAdjacentFrames (method, args, timeout) {
+  let requestAttrs = {method: method, args: args};
+  let originalIncomingRequest = incomingRequests.find(requestAttrs);
+  let frames = getAdjacentFrames().filter(function (adjacentFrame) {
+    if (originalIncomingRequest) {
+      return originalIncomingRequest.contentWindow !== adjacentFrame;
+    } else {
       return true;
-    });
-    if (!adjacentFrames.length) {
-      reject();
     }
-    adjacentFrames.forEach(function (frame) {
-      sendRpcRequest(frame, method, args)
-      .then(function (response) {
-        resolve();
-      })
-      .catch(function () {
-        failedAttempts += 1;
-        if (failedAttempts = adjacentFrames.length) {
-          reject();
-        }
-      });
-    });
   });
+  let outgoingRequest = new OutgoingRequest(requestAttrs, timeout);
+  outgoingRequests.add(outgoingRequest);
+  frames.forEach(function (frame) {
+    sendRpcRequest(frame, method, args);
+  });
+  return outgoingRequest.promise;
 }
 
 // start listening for messages from adjacent frames
@@ -160,5 +128,6 @@ window.addEventListener('message', handleMessage);
 
 // export public methods
 module.exports = {
+  resolveRequest: resolveRequest,
   tryAdjacentFrames: tryAdjacentFrames
 }
